0f4d1a16f6c32e08da2f306759978bc73c7a21b3
dgush <dgushch@gmail.com>
2024-11-27 18:30:46 +0400

    1.0.0

--- bot/main.py
+++ bot/main.py
@@ -8,7 +8,6 @@ from handlers import dp
 from config import Config
 from init import set_main_menu, bot, log_error
 from db.base import init_models
-from db.temp import update_db
 from utils.scheduler_utils import scheduler_start_async, hand_orders, hand_cashback_orders
 from utils.wallet_utils import test_wallet_valid
 
@@ -16,16 +15,15 @@ from utils.wallet_utils import test_wallet_valid
 async def main() -> None:
     # await test_wallet_valid()
     await init_models()
-    await update_db()
-    # await set_main_menu()
-    # if Config.debug:
-    #     await hand_orders()
-    #     await hand_cashback_orders()
-    # else:
-    #     await scheduler_start_async()
-    #
-    # await bot.delete_webhook (drop_pending_updates=True)
-    # await dp.start_polling(bot)
+    await set_main_menu()
+    if Config.debug:
+        await hand_orders()
+        await hand_cashback_orders()
+    else:
+        await scheduler_start_async()
+
+    await bot.delete_webhook (drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 
 if __name__ == "__main__":
