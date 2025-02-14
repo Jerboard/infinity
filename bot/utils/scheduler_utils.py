acf529b239525f599f7d5f51301876a9d3c22584
dgush <dgushch@gmail.com>
2025-02-14 13:10:25 +0400

    1.0.24

--- bot/utils/scheduler_utils.py
+++ bot/utils/scheduler_utils.py
@@ -9,7 +9,7 @@ async def scheduler_start_async():
     # scheduler_async.add_job(check_sub, "cron", hour=22)
     scheduler.add_job(update_currency_rate, "interval", minutes=5)
     scheduler.add_job(del_old_orders, "interval", minutes=1)
-    scheduler.add_job(hand_orders, "interval", seconds=20)
+    scheduler.add_job(hand_orders, "interval", seconds=5)
     scheduler.add_job(hand_cashback_orders, "interval", seconds=20)
     # scheduler.add_job(send_doc, "interval", seconds=20)
     scheduler.start()
